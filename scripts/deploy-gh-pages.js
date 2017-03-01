bea1dfb5d3d161e7030a34307ed951226514f7b6
Naomi Carrillo Plasencia <alu0100829914@ull.edu.es>
2017-03-01 16:24:09 +0000

    Solucionado error de despliegue en gh-pages

--- scripts/deploy-gh-pages.js
+++ scripts/deploy-gh-pages.js
@@ -1,6 +1,7 @@
 var ghpages = require('gh-pages');
 var path = require('path');
  
-ghpages.publish(path.join(__dirname, '/_book'), function(err) {
+ghpages.publish(path.join(__dirname, '../_book'), function(err) {
+    console.log(err);
     console.log("publicación finalizada");
 });
